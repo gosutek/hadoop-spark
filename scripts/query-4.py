d18b264940c65ef615dcc01f08be04967e89dca7
gosutek <gosutek@gmail.com>
2024-01-15 21:50:47 +0200

    Added query 4.1

--- scripts/query-4.py
+++ scripts/query-4.py
@@ -6,12 +6,25 @@ import geopy.distance
 
 export_path = 'hdfs://advdb-master:54310/user/master/exports/'
 stations_path = 'hdfs://advdb-master:54310/user/master/other_data/LAPD_Police_Stations.csv'
+lib_path = 'hdfs://advdb-master:54310/user/master/lib/'
 
-def get_distance(lat1, long1, lat2, long2):
-    return geopy.distance.geodesic((lat1, long1), (lat2, long2)).km
+def get_distance(lat1, lon1, lat2, lon2):
+    return geopy.distance.geodesic(( float(lat1), float(lon1) ), ( float(lat2), float(lon2) )).km
+
+def min_distance(lat1, lon1):
+    _min = float('inf')
+    for pair in pd_coords.keys():
+        dst = geopy.distance.geodesic((lat1, lon1), pair).km
+        if (dst < _min):
+            _min = dst
+            min_pair = pair
+    return [ _min, pd_coords[min_pair] ]
 
 spark = SparkSession.builder \
         .appName('query-4').getOrCreate()
+
+spark.sparkContext.addPyFile(lib_path + 'geopy.zip')
+spark.sparkContext.addPyFile(lib_path + 'geographiclib.zip')
 
 query4_df = spark.read.format('csv').option('header', 'true').load(export_path)
 
@@ -21,11 +34,24 @@ stations = spark.read \
         .option('inferSchema', 'true') \
         .load(stations_path)
 
-stations.printSchema()
+pd_coords = { (row['Y'], row['X']) : row['DIVISION'] for row in stations.collect() }
 query4_df = query4_df.filter(query4_df['Weapon Used Cd'].startswith('1'))
 query4_df = query4_df.withColumn('AREA', query4_df['AREA'].cast('Integer'))
-query4_df = query4_df.join(stations, query4_df['AREA'] == stations['PREC'], 'inner')
-query4_df = query4_df.groupBy(year('Date Rptd').alias('year')) \
-        .agg(count('DR_NO').alias('#')) \
+query4_df = query4_df.withColumn('LAT', query4_df['LAT'].cast(DoubleType()))
+query4_df = query4_df.withColumn('LON', query4_df['LON'].cast(DoubleType()))
+query4_df = query4_df.filter(( query4_df['LAT'] != 0 ) & ( query4_df['LON'] != 0 ))
+# Query4-1
+join_1 = query4_df.join(stations, query4_df['AREA'] == stations['PREC'], 'inner')
+join_1 = join_1.withColumn('pd_distance', \
+                                 udf(lambda lat1, lon1, lat2, lon2: \
+                                     get_distance(lat1, lon1, lat2, lon2)) \
+                                 ( join_1.LAT, join_1.LON, join_1.Y, join_1.X ))
+query4_1_df = join_1.groupBy(year('Date Rptd').alias('year')) \
+        .agg(round(avg('pd_distance'), 3).alias('average_distance'), count('DR_NO').alias('#')) \
         .orderBy(col('year').asc())
-query4_df.show(5)
+query4_1_df.show(5)
+# Query4-2
+query4_2_df = join_1.groupBy(col('DIVISION').alias('division')) \
+        .agg(round(avg('pd_distance'), 3).alias('average_distance'), count('DR_NO').alias('#')) \
+        .orderBy(col('division').desc())
+query4_2_df.show(5)
